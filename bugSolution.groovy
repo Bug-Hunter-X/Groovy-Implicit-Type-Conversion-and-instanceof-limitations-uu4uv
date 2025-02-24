697ec7a577d3f0405f242daa44da90f9cd5cc7ec
BugHunter-X <askzenai@gmail.com>
2025-02-24 11:26:07 +0100

    Add bugSolution.groovy file

---
+++ bugSolution.groovy
@@ -0,0 +1,20 @@
+```groovy
+def myMethod(param) {
+  if (param == null) {
+    return "Null value"
+  } else if (param instanceof String) {
+    return param.toUpperCase()
+  } else if (param instanceof Integer) {
+    return param * 2
+  } else if (param instanceof List && param.every{it instanceof Integer}) {
+    return param.sum() * 2
+  } else {
+    return "Unsupported type"
+  }
+}
+
+println myMethod(null) // Output: Null value
+println myMethod("hello") // Output: HELLO
+println myMethod(5) // Output: 10
+println myMethod([1,2,3]) // Output: 12
+```
